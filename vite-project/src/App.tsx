1ebdea706023761ede1a8bef14d0e79539e45510
Maelys Ami <maelys.ami@agence-dbcom.fr>
2025-03-20 14:24:48 +0100

    Remove: unused info

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -45,17 +45,6 @@ function App() {
           <img src="src/assets/logo.png" className="logo" alt="logo" />
       </div>
       <h1>Dispatch</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
 
       <div>
         <h1>Secret Key</h1>
