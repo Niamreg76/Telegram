3f8c49ba1fa3a8da2f8568d39df8e214d606443f
Maelys Ami <maelys.ami@agence-dbcom.fr>
2025-03-20 14:23:23 +0100

    Add logo

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -42,12 +42,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src="src/assets/logo.png" className="logo" alt="logo" />
       </div>
       <h1>Dispatch</h1>
       <div className="card">
